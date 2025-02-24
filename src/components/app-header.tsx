5a1e9c3c81665b6d60c6aaa8042d36aafd373545
arkarlin2003 <arkarlin486@gmail.com>
2025-02-24 19:44:12 +0630

    feat: mobile view landing page

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -5,12 +5,12 @@ import { Search } from "lucide-react";
 
 const AppHeader = () => {
   return (
-    <header className="h-16 shadow-md w-full  px-7 flex items-center justify-between fixed top-0 left-0">
+    <header className="h-16 shadow-md w-full max-sm:px-5  px-7 flex items-center justify-between fixed top-0 left-0">
       <div className="flex items-center gap-2">
         <img src={Logo} alt="ai-solution-logo" />
-        <h1 className="text-xl">Ai-Solution</h1>
+        <h1 className="text-xl max-sm:text-lg">Ai-Solution</h1>
       </div>
-      <ul className="flex items-center space-x-10">
+      <ul className="flex items-center space-x-10 max-sm:hidden">
         {navLinks.map((link, i) => (
           <li key={i}>
             <p className=" font-medium">{link}</p>
@@ -18,8 +18,8 @@ const AppHeader = () => {
         ))}
       </ul>
       <div className="flex items-center gap-2">
-        <Button className="btn-bg">Contact us</Button>
-        <Button className="btn-bg w-28">
+        <Button className="btn-bg max-sm:text-xs">Contact us</Button>
+        <Button className="btn-bg w-28 max-sm:text-xs">
           <Search />
         </Button>
       </div>
